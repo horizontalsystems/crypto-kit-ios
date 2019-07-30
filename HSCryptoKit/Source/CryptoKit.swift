780fef76affbe4990cf259bce35fc67436533b94
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2019-07-30 16:52:44 +0600

    Add compactSign to CryptoKit

--- HSCryptoKit/Source/CryptoKit.swift
+++ HSCryptoKit/Source/CryptoKit.swift
@@ -75,7 +75,33 @@ public struct CryptoKit {
         
         return der
     }
-    
+
+    public static func compactSign(_ data: Data, privateKey: Data) throws -> Data {
+        precondition(data.count > 0, "Data must be non-zero size")
+        precondition(privateKey.count > 0, "PrivateKey must be non-zero size")
+
+        let ctx = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN))!
+        defer { secp256k1_context_destroy(ctx) }
+
+        let signature = UnsafeMutablePointer<secp256k1_ecdsa_signature>.allocate(capacity: 1)
+        defer { signature.deallocate() }
+        let status = data.withUnsafeBytes { (ptr: UnsafePointer<UInt8>) in
+            privateKey.withUnsafeBytes { secp256k1_ecdsa_sign(ctx, signature, ptr, $0, nil, nil) }
+        }
+        guard status == 1 else { throw CryptoKitError.signFailed }
+
+        let normalizedsig = UnsafeMutablePointer<secp256k1_ecdsa_signature>.allocate(capacity: 1)
+        defer { normalizedsig.deallocate() }
+        secp256k1_ecdsa_signature_normalize(ctx, normalizedsig, signature)
+
+        let length: size_t = 64
+        var compact = Data(count: length)
+        guard compact.withUnsafeMutableBytes({ return secp256k1_ecdsa_signature_serialize_compact(ctx, $0, normalizedsig) }) == 1 else { throw CryptoKitError.noEnoughSpace }
+        compact.count = length
+
+        return compact
+    }
+
     public static func createPublicKey(fromPrivateKeyData privateKeyData: Data, compressed: Bool = false) -> Data {
         precondition(privateKeyData.count > 0, "PrivateKeyData must be non-zero size")
         // Convert Data to byte Array
